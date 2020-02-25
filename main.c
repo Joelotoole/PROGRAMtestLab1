c7723f4f5e837261c75cc09c1b5f66e94c050a27
Joel O'Toole <c3350177@uon.edu.au>
2020-02-25 13:42:03 +1100

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float x;
+    float y;
+    printf("Enter a Number: ");
+    scanf("%f", &x);
+    y =x/1-x;
+    printf("y: %f\n", y);
+    return 0;
+}
+//StackOverflow
